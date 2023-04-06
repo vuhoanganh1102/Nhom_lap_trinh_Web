e913a68fefa380ca7856580e3be828f767137424
MrLoran <anhvh1102@gmail.com>
2023-04-06 19:19:34 +0700

    done slider

--- assets/on_off_taskbar.js
+++ assets/on_off_taskbar.js
@@ -1,8 +1,16 @@
 function on_taskbar(){
     var getTask=document.getElementsByClassName("box");
+    var getPrev=document.getElementsByClassName("prev");
+    var getNext=document.getElementsByClassName("next");
     getTask[0].style.display='block';
+    getPrev[0].style.display='none';
+    getNext[0].style.display='none';
 }
 function off_taskbar(){
     var putOff= document.getElementsByClassName("box");
+    var getPrev=document.getElementsByClassName("prev");
+    var getNext=document.getElementsByClassName("next");
     putOff[0].style.display='none';
+    getPrev[0].style.display='block';
+    getNext[0].style.display='block';
 }
