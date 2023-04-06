c73e28489cb01ea66b5e4762aca70f8a0789a012
MrLoran <anhvh1102@gmail.com>
2023-04-06 18:10:09 +0700

    done responsive header

---
+++ assets/on_off_taskbar.js
@@ -0,0 +1,8 @@
+function on_taskbar(){
+    var getTask=document.getElementsByClassName("box");
+    getTask[0].style.display='block';
+}
+function off_taskbar(){
+    var putOff= document.getElementsByClassName("box");
+    putOff[0].style.display='none';
+}
